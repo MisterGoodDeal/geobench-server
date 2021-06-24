f55242a212cd4c2495ca795c865eb063a04ba3ff
MisterGoodDeal <pro@turtletv.fr>
2021-06-24 15:27:00 +0200

    feat: 🚀Project init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { benches } from "./routes/bench";
+const env = require("dotenv").config();
+
+var express = require("express");
+var app = express();
+app.disable("etag"); // No caching
+
+app.get("/", function (req: any, res: any) {
+  console.log("Getting base");
+  res.send("Geobench API server");
+});
+
+benches();
+
+app.listen(process.env.EXPRESS_PORT);
