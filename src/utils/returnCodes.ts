5f010d42613db71ec7cbc02e7b3abb394fd54bab
MisterGoodDeal <pro@turtletv.fr>
2021-06-25 23:11:35 +0200

    feat: Adding return codes

--- src/utils/returnCodes.ts
+++ src/utils/returnCodes.ts
@@ -1,23 +1,75 @@
 export const returnCode = {
+  missingParameters: {
+    code: 400,
+    payload: {
+      title: "missing_parameters",
+      message: "Missing parameters!",
+    },
+  },
   unknownUser: {
     code: 400,
     payload: {
       title: "unknown_user",
-      error: "Unknown users!",
+      message:
+        "Unknown user! The user you trying to get insn't present in the database.",
+    },
+  },
+  alreadyUser: {
+    mail: {
+      code: 400,
+      payload: {
+        title: "already_exists_mail",
+        message: "The user you're trying to add already exists in the database",
+      },
+    },
+    username: {
+      code: 400,
+      payload: {
+        title: "already_exists_username",
+        message: "The user you're trying to add already exists in the database",
+      },
+    },
+    both: {
+      code: 400,
+      payload: {
+        title: "already_exists_both",
+        message: "The user you're trying to add already exists in the database",
+      },
+    },
+  },
+  userCreated: {
+    code: 201,
+    payload: {
+      title: "user_created",
+      message: "The user has been created successfully!",
+    },
+  },
+  user: {
+    wrongPassword: {
+      code: 400,
+      payload: {
+        title: "wrong_password",
+        message: "Password missmatch!",
+      },
     },
   },
   unauthorized: {
     code: 401,
     payload: {
       title: "unauthorized",
-      error: "You need to authetificate before using this route!",
+      message: "You need to authetificate before using this route!",
     },
   },
   internalError: {
     code: 500,
     payload: {
       title: "internal_server_error",
-      error: "Internal server error! Check logs",
+      message: "Internal server error! Check logs",
     },
   },
 };
+
+export interface Res {
+  status: (code: number) => any;
+  json: (json: any) => any;
+}
