e073197753aa670b6dbe4beceb0d9b429a9ae566
MisterGoodDeal <pro@turtletv.fr>
2021-06-25 23:13:02 +0200

    feat: Adding helper function

---
+++ src/utils/helpers.ts
@@ -0,0 +1,13 @@
+export const generateRandomString = (len?: number): string => {
+  const length = len === undefined ? 16 : len;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
