bf4f311eb0f85e23de73c9f86d4c9daef3288411
MisterGoodDeal <pro@turtletv.fr>
2021-06-25 23:14:13 +0200

    feat: 👨v1.0.0 of `user` routes and its utils

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,11 +1,11 @@
 import { db } from "../db";
+// Fonctions
+
 const userExists = async (id: number): Promise<boolean> => {
   const rows = await db.queryParams(
     "SELECT COUNT(*) as userExist FROM `users` WHERE `id` = ?",
     [id]
   );
-
-  //return rows[0].userExist === 1;
   if (rows[0].userExist === 1) {
     return true;
   } else {
@@ -13,20 +13,69 @@ const userExists = async (id: number): Promise<boolean> => {
   }
 };
 
-export const user = {
-  exists: userExists,
+const getUserRegister = async (login: string): Promise<User> => {
+  const rows = await db.queryParams(
+    "SELECT DISTINCT * FROM `users` WHERE `mail` = ? OR `pseudo` = ?",
+    [login, login]
+  );
+  const user: User = rows[0];
+  return user;
 };
 
+const checkRegisterUserExist = async (
+  email: string,
+  username: string
+): Promise<CheckUserRegisterExist> => {
+  const mail = await db.queryParams(
+    "SELECT DISTINCT COUNT(*) as userExist FROM `users` WHERE `mail` = ?",
+    [email]
+  );
+  const pseudo = await db.queryParams(
+    "SELECT DISTINCT COUNT(*) as userExist FROM `users` WHERE `pseudo` = ?",
+    [username]
+  );
+  return {
+    email: mail[0].userExist === 1 ? true : false,
+    username: pseudo[0].userExist === 1 ? true : false,
+  };
+};
+
+export const user = {
+  exists: userExists,
+  existsRegister: checkRegisterUserExist,
+  info: getUserRegister,
+};
+
+// Interfaces
+
 export interface User {
-  id: number;
-  is_ads: number;
-  is_banned: number;
-  is_deleted: number;
+  id?: number;
+  is_ads?: number;
+  is_banned?: number;
+  is_deleted?: number;
   prenom: string;
   nom: string;
   mail: string;
   pseudo: string;
-  mdp: string;
+  mdp?: string;
   favoris: string;
   reset_key: string;
+}
+
+export interface UserRegister {
+  prenom: string;
+  nom: string;
+  email: string;
+  username: string;
+  password: string;
+}
+
+export interface UserLogin {
+  login: string;
+  password: string;
+}
+
+interface CheckUserRegisterExist {
+  email: boolean;
+  username: boolean;
 }
