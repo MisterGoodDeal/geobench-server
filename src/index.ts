d1c4f1d71d06bd4ec23ec659e0103d8fed1520cf
Milan Camus <pro@turtletv.fr>
2021-07-30 23:17:10 +0200

    fix: 😔Rollback on removing HTTP server

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,13 @@ benches(app);
 user(app);
 community(app);
 
+// Starting both http & https servers
+const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+  console.log("HTTP Server running on port 80");
+});
 
 httpsServer.listen(443, () => {
   console.log("HTTPS Server running on port 443");
